95e7450a7c6b1fd4f1b661f5046ac40fae51ac6f
Jessica Carvalho <jmaria.carvalho30@gmail.com>
2024-08-02 14:30:17 -0300

    index

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  
-    path('formulario/', views.formulario, name='formulario'), 
-    path('quem_somos/', views.quem_somos, name='quem_somos'),  
-]
-
